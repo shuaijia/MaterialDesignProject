d28d2a57012131a17283dce0073da604aba58e6c
shuajia <819418850@qq.com>
2017-09-19 16:16:18 +0800

    jia:

--- app/src/main/java/com/jia/mddemo/activity/AboutMeActivity.java
+++ app/src/main/java/com/jia/mddemo/activity/AboutMeActivity.java
@@ -1,6 +1,5 @@
 package com.jia.mddemo.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
