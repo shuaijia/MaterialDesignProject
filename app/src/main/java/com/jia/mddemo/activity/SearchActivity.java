44c2176e244897e2497c8d84aca45337b3ced3ea
shuajia <819418850@qq.com>
2017-12-15 11:47:28 +0800

    jia：修改

--- app/src/main/java/com/jia/mddemo/activity/SearchActivity.java
+++ app/src/main/java/com/jia/mddemo/activity/SearchActivity.java
@@ -27,6 +27,7 @@ public class SearchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_search);
 
